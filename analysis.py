d49831a21e63503912c57860841e803b8086e94c
georgefox19771978 <georgefox19771978@gmail.com>
2024-11-14 13:50:48 -0500

    Create analysis.py
    first commit

---
+++ analysis.py
@@ -0,0 +1,12 @@
+# analysis.py
+import pandas as pd
+
+def load_and_clean_data(url):
+    df = pd.read_csv(url)
+    df.dropna(inplace=True)
+    return df
+
+if __name__ == "__main__":
+    url = "https://example.com/data.csv"
+    df = load_and_clean_data(url)
+    print(df.head())
